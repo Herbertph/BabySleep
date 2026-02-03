a3b15e148e39b1f2fee745aee9e9b0f8484051c4
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2026-02-03 16:30:35 -0400

    Initial Commit

---
+++ BabySleep/BabySleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySleepApp.swift
+//  BabySleep
+//
+//  Created by Herbert Parasio on 2026-02-03.
+//
+
+import SwiftUI
+
+@main
+struct BabySleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
